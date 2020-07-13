30a117de1d53b8ed79b2a413d4f9529eb2bbc39a
Oluwamayowa Esan <oesan@asu.edu>
2020-07-13 07:36:29 -0700

    remove some error catching

--- util/util.js
+++ util/util.js
@@ -4,47 +4,42 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     async fetchCurrentURL() {
-        try {
-            const res = await get('https://www.robotevents.com/robot-competitions/vex-robotics-competition');
-            const $ = cheerio.load(res.data);
-            const url = $('i.fa-question').parent().attr('href');
-            return url;
-        } catch (e) { return console.log(e); }
+        const res = await get('https://www.robotevents.com/robot-competitions/vex-robotics-competition');
+        const $ = cheerio.load(res.data);
+        const url = $('i.fa-question').parent().attr('href');
+        return url;
+
     },
     async fetchPageCount(url) {
-        try {
-            const res = await get(`${url}`);
-            const $ = cheerio.load(res.data);
-            const pageCount = $('.pagination', '.panel-body').find('li').length - 2;
-            return pageCount;
-        } catch (e) { return console.log(e); }
+        const res = await get(`${url}`);
+        const $ = cheerio.load(res.data);
+        const pageCount = $('.pagination', '.panel-body').find('li').length - 2;
+        return pageCount;
     },
     async fetchUnansweredQuestions(url) {
-        try {
-            const base = new Map();
-            const pageCount = await module.exports.fetchPageCount(url);
+        const base = new Map();
+        const pageCount = await module.exports.fetchPageCount(url);
 
-            for (let i = 1; i <= pageCount; i++) {
+        for (let i = 1; i <= pageCount; i++) {
 
-                const res = await get(`${url}?page=${i}`);
-                const $ = cheerio.load(res.data);
-                const questionTitles = $('.panel-body').children('h4.title:not(:has(a span))');
+            const res = await get(`${url}?page=${i}`);
+            const $ = cheerio.load(res.data);
+            const questionTitles = $('.panel-body').children('h4.title:not(:has(a span))');
 
-                questionTitles.each((index, child) => {
-                    //ps i hate scraping
-                    const title = module.exports.unformat($(child).text());
-                    const author = module.exports.unformat($(child).nextUntil('hr').children('.details').children('.author').text());
-                    const timestamp = module.exports.unformat($(child).nextUntil('hr').children('.details').children('.timestamp').text());
-                    const tags = module.exports.unformat($(child).next().next('.tags').text(), false);
-                    const url = $(child).children('a').attr('href');
-                    const id = url.match(/QA\/(\d+)/)[1];
+            questionTitles.each((index, child) => {
+                //ps i hate scraping
+                const title = module.exports.unformat($(child).text());
+                const author = module.exports.unformat($(child).nextUntil('hr').children('.details').children('.author').text());
+                const timestamp = module.exports.unformat($(child).nextUntil('hr').children('.details').children('.timestamp').text());
+                const tags = module.exports.unformat($(child).next().next('.tags').text(), false);
+                const url = $(child).children('a').attr('href');
+                const id = url.match(/QA\/(\d+)/)[1];
 
-                    base.set(id, { title, author, timestamp, url, tags });
+                base.set(id, { title, author, timestamp, url, tags });
 
-                });
-            }
-            return base;
-        } catch (e) { return console.log(e); }
+            });
+        }
+        return base;
     },
     generateQuestionEmbeds(ids, questions) {
         try {
